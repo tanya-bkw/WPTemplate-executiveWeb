7c4edc67f1db66372a34271498d23d64b52277ea
tanya-bkw <taanbkAD@gmail.com>
2017-03-22 18:40:20 +0100

    repaired

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var sourcemaps  = require('gulp-sourcemaps');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
-//    browserSync.init({
-//        server: "http://localhost/CodersLab/LOD_FRO_S_02/Wordpress/"
-//    });
+    browserSync.init({
+        proxy: "http://localhost/wordpress/"
+    });
 
     gulp.watch("sass/*.scss", ['sass']);
-    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch("*.php").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
@@ -18,6 +19,7 @@ gulp.task('sass', function() {
     return gulp.src("sass/*.scss")
         .pipe(sass())
         .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest("./"))
         .pipe(browserSync.stream());
 });
